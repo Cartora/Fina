29ecd398634230f654fba6980d3a26c1757a5251
Tomer Peceniski <tomerpeceniski@gmail.com>
2025-04-10 12:46:22 +0300

    Refactor/fine checking function (#13)
    * refactor: define s3 deletion policy
    
    * refactor: update fine checking logic

--- car-lambdas/FineCheckingFunction/src/main/java/fine/FineCheckingLambda.java
+++ car-lambdas/FineCheckingFunction/src/main/java/fine/FineCheckingLambda.java
@@ -5,52 +5,59 @@ import com.amazonaws.services.lambda.runtime.events.DynamodbEvent;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
-import com.amazonaws.services.dynamodbv2.model.AttributeValue;
-import com.amazonaws.services.dynamodbv2.model.PutItemRequest;
+import com.amazonaws.services.dynamodbv2.model.*;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Random;
 
 public class FineCheckingLambda implements RequestHandler<DynamodbEvent, Void> {
 
-    private static final double FINE_PROBABILITY = 0.15;
-    private static final String TABLE_NAME = System.getenv("FINE_CANDIDATES_TABLE");
+    private static final String FINE_CANDIDATES_TABLE = System.getenv("FINE_CANDIDATES_TABLE");
 
     private final AmazonDynamoDB dynamoDB = AmazonDynamoDBClientBuilder.defaultClient();
-    private final Random random = new Random();
 
     @Override
     public Void handleRequest(DynamodbEvent event, Context context) {
         for (DynamodbEvent.DynamodbStreamRecord record : event.getRecords()) {
             var newImage = record.getDynamodb().getNewImage();
-            if (newImage == null)
-                continue;
+            if (newImage == null) continue;
 
             String carPlate = newImage.get("carPlate").getS();
             String slotId = newImage.get("slotId").getS();
             String timestamp = newImage.get("timestamp").getS();
 
-            boolean hasFine = random.nextDouble() < FINE_PROBABILITY;
-
-            if (hasFine) {
-                context.getLogger().log("Car " + carPlate + " already has a fine.\n");
-            } else {
-                context.getLogger()
-                        .log("Car " + carPlate + " does not have a fine. Adding to FineCandidates table...\n");
-
-                Map<String, AttributeValue> item = new HashMap<>();
-                item.put("slotId", new AttributeValue(slotId));
-                item.put("carPlate", new AttributeValue(carPlate));
-                item.put("validationTimestamp", new AttributeValue(timestamp));
-
-                PutItemRequest request = new PutItemRequest()
-                        .withTableName(TABLE_NAME)
-                        .withItem(item);
-
-                dynamoDB.putItem(request);
+            if (isAlreadyCandidate(slotId, carPlate)) {
+                context.getLogger().log("Car " + carPlate + " is already in FineCandidates. Skipping...\n");
+                continue;
             }
+
+            context.getLogger().log("Car " + carPlate + " is not yet in FineCandidates. Inserting...\n");
+
+            Map<String, AttributeValue> item = new HashMap<>();
+            item.put("slotId", new AttributeValue(slotId));
+            item.put("carPlate", new AttributeValue(carPlate));
+            item.put("validationTimestamp", new AttributeValue(timestamp));
+
+            PutItemRequest request = new PutItemRequest()
+                    .withTableName(FINE_CANDIDATES_TABLE)
+                    .withItem(item);
+
+            dynamoDB.putItem(request);
         }
+
         return null;
+    }
+
+    private boolean isAlreadyCandidate(String slotId, String carPlate) {
+        Map<String, AttributeValue> key = new HashMap<>();
+        key.put("slotId", new AttributeValue(slotId));
+        key.put("carPlate", new AttributeValue(carPlate));
+
+        GetItemRequest request = new GetItemRequest()
+                .withTableName(FINE_CANDIDATES_TABLE)
+                .withKey(key);
+
+        Map<String, AttributeValue> item = dynamoDB.getItem(request).getItem();
+        return item != null;
     }
 }
